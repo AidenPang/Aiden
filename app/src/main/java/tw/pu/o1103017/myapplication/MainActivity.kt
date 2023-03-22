992dc3439c85007c5a2f09526dc656162079bac3
student <zhen030510@gmail.com>
2023-03-22 14:17:00 +0800

    v2彭定振

--- app/src/main/java/tw/pu/o1103017/myapplication/MainActivity.kt
+++ app/src/main/java/tw/pu/o1103017/myapplication/MainActivity.kt
@@ -2,10 +2,23 @@ package tw.pu.o1103017.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var txv:TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        txv = findViewById(R.id.txv)
+        txv.setOnClickListener (){
+            if (txv.text == "你好不好吗？"){
+                txv.text = "不想知道"
+            }else{
+                txv.text = "哈哈啊哈哈啊哈"
+            }
+
+        }
     }
 }
